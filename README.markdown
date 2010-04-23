7bc9ba9011d7cee35a407b96ef1185e5b57be483
Carson Sasser <sassercw@cox.net>
2010-04-23 18:46:42 -0500

    initial commit

---
+++ README.markdown
@@ -0,0 +1,21 @@
+Markdown Guide Extension
+=======================
+
+* Version: 0.1 Beta
+* Author: Carson Sasser (sassercw@cox.net)
+* Build Date: 22 April 2010
+* Requirements: Only tested on Symphony 2.0.8RC1
+
+Installation
+------------
+
+1. Install the four files in a folder named 'markdown_guide' inside your Symphony 'extensions' folder.
+
+2. Enable it by selecting the *Markdown Guide* extension, choose Enable from the with-selected menu, and then click Apply.
+
+3. Now the condensed guide will appear below any textarea field that uses one of the markdown text formatter options.
+
+TODO
+----
+
+* Add an option to the textarea field to suppress the guide even when a markdown text formatter is being used.
---
+++ extension.driver.php
@@ -0,0 +1,49 @@
+<?php
+
+Class extension_markdown_guide extends Extension {
+  
+	// Simply outputs information to Symphony about the extension
+	public function about() {
+		$info = array(
+			'author' => array(
+				'email' => 'sassercw@cox.net',
+				'name' => 'Carson Sasser',
+				'website' => 'http://carsonsasser.com/'
+			),
+			'name' => 'Markdown Guide',
+			'release-date' => '2010-04-20',
+			'version' => '0.1'
+		);
+		return $info;
+	}
+	
+	public function getSubscribedDelegates() {
+		return array(
+			array(
+			'page' => '/backend/',
+			'delegate' => 'ModifyTextareaFieldPublishWidget',
+			'callback' => 'addGuideBelowTextArea'
+			),
+		);
+	}
+	
+	public function addGuideBelowTextArea($pointer) {
+		//only show guide when using markdown
+		$formatter = substr($pointer['field']->get('formatter'), 0,  11);
+		if ($formatter != 'pb_markdown') return;
+
+		//append the textarea here so the guide will show after the textarea in the form
+		$pointer['label']->appendChild($pointer['textarea']);
+
+		//nullify the textarea to prevent another one being appended in field.textarea.php
+		$pointer['textarea'] = Widget::Label('');
+
+		//retrieve the guide and append it
+		$file = DOCROOT . '/extensions/markdown_guide/guide.txt';
+		$contents = file_get_contents($file);
+		$guide = Widget::Label($contents);
+		$pointer['label']->appendChild($guide);
+	}
+}
+
+?>
