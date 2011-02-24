bb3f9f8d27051a4b9b4e7b842b2ab3e4e730c912
Marcio Toledo <mt@marciotoledo.com>
2011-02-24 02:03:01 -0300

    Translation: Portuguese (Brazil)

---
+++ lang/lang.pt-br.php
@@ -0,0 +1,112 @@
+<?php
+
+	$about = array(
+		'name' => 'Portuguese (Brazil)',
+		'author' => array(
+			'name' => 'Marcio Toledo',
+			'email' => 'mt@marciotoledo.com',
+			'website' => 'http://marciotoledo.com'
+		),
+		'release-date' => '2011-02-24'
+	);
+	
+	
+	/*
+	 * EXTENSION: Markdown Guide
+	 * Localisation strings
+	 */
+
+	$dictionary = array(
+		
+		'Show Markdown Guide' =>
+		'Mostrar Guia Markdown (Formatação de Texto)',
+		
+		'Hide Markdown Guide' =>
+		'Ocultar Guia Markdown (Formatação de Texto)',
+		
+		'Headers' => 
+		'Cabeçalhos',
+		
+		'Precede with a # for each level desired as in: ### This is an h3 header' => 
+		'Precedido com um # para cada nível desejado, exemplo: ### Este é um cabeçalho h3',
+		
+		'Paragraphs' => 
+		'Parágrafos',
+		
+		'Seperate with a blank line' => 
+		'Separar com uma linha em branco',
+		
+		'Blockquotes' => 
+		'Bloco de Citação',
+		
+		'Precede with a >  (two >> will produce a quote within a quote)' => 
+		'Precedido com um > (dois >> irá produzir uma citação dentro de uma citação)',
+		
+		'Unorderd lists' => 
+		'Lista Desordenada',
+		
+		'Precede list items with a *, + or -' => 
+		'Prededido com um *, + ou - cada ítem da lista',
+		
+		'Ordered lists' => 
+		'Lista Ordenada',
+		
+		'Precede list items with a number followed by a period' => 
+		'Precedido com um número seguido de uma vírgula cada ítem da lista',
+		
+		'Emphasis' => 
+		'Ênfase',
+		
+		'*italics*  **bold**  or  _italics_  __bold__' => 
+		'*itálico*, **negrito** ou _itálico_, __negrito__',
+		
+		'Links' => 
+		'Links',
+		
+		'[link text](http://some-site.com "optional title")' => 
+		'[Texto do link](http://algumsite.com.br "título opcional")',
+		
+		'Automatic links' => 
+		'Links Automáticos',
+		
+		'Enclose a URL in angle brackets to produce a link using the URL itself as the link text' => 
+		'Incluir uma URL entre colchetes para produzir um link usando a própria URL como texto do link',
+		
+		'Images' => 
+		'Imagens',
+		
+		'![alternate text](/path/to/img.jpg "optional title")' => 
+		'![texto alternativo](/caminho/da/imagem.jpg "título opcional")',
+		
+		'Code blocks' => 
+		'Bloco de Códigos',
+		
+		'Indent each line of the block by at least 4 spaces or one tab' => 
+		'Identar cada linha do bloco com pelo menos 4 espaços ou 1 tab',
+		
+		'Inline code' => 
+		'Código em linha',
+		
+		'Wrap the code with backtick characters' => 
+		'Envolva o código com caracteres crase, exemplo `meuCódigo()`',
+		
+		'Horizontal rules' => 
+		'Linhas horizontais',
+		
+		'Place 3 or more asterisks, hyphens or underscores on a line by itself' => 
+		'Coloque 3 ou mais asteriscos, hífens ou sublinhado em uma linha somente, exemplo ---',
+		
+		'Line breaks' => 
+		'Quebra de linha',
+		
+		'To force a line break, end the line with 2 spaces and then start a new line' => 
+		'Para forçar uma quebra de linha, termine a linha com 2 espaços e então inicie uma nova linha',
+		
+		'Backslash escapes' => 
+		'Caracter de Escape',
+		
+		'To force a literal display of special markdown characters, precede with a backslash' => 
+		'Para forçar um caracter especial de marcação, ele deve vir precedido de uma barra invertida, exemplo \*',
+
+
+	);
